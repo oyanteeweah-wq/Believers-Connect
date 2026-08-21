aa1c687408b50a796805017c83de4a7e398f8490
oyanteeweah-wq <oyanteeweah@gmail.com>
2026-08-20 22:33:47 -0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL='https://zupbqtnqbsmlwbidsddk.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY='sb_publishable_NtyX9tGZ7vAmLB73ylIasw_RZZf7P2o';
+
+const supabaseClient=window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
